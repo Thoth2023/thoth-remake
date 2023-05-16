cc15dc43042053f776285ff898f4061fb72aaeeb
gpmatheus <gp.matheusluis@gmail.com>
2023-05-16 12:14:44 -0300

    updated with develop

---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Thoth</title>
+
+        <!-- CSS only -->
+        <link id="pagestyle" href="../assets/css/argon-dashboard.css" rel="stylesheet" />
+    </head>
+    <body>
+        <button type="button" class="btn bg-gradient-success">Success</button>
+    </body>
+</html>
