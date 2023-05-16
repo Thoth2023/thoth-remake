542807c2d061df13c95d1bb16136b8269cf03938
jauzer <jauzerv@outlook.com>
2023-05-16 19:32:56 -0300

    fix model project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -11,5 +11,40 @@ class Project extends Model
     // we need to specify the table name
     protected $table = 'project';
 
+    public $timestamps = false;
+
     use HasFactory;
+
+    protected $fillable = [
+        'id_user',
+        'title', 
+        'description', 
+        'objectives',
+        //'copy_planning',
+    ];
+
+    private function insertSearchStringGenerics($idProject)
+    {
+        // Insert logic for search_string_generics table
+    }
+
+    private function insertSearchStrategy($idProject)
+    {
+        // Insert logic for search_strategy table
+    }
+
+    private function insertInclusionRule($idProject)
+    {
+        // Insert logic for inclusion_rule table
+    }
+
+    private function insertExclusionRule($idProject)
+    {
+        // Insert logic for exclusion_rule table
+    }
+
+    private function insertMembers($idProject, $createdBy, $name)
+    {
+        // Insert logic for members table
+    }
 }
