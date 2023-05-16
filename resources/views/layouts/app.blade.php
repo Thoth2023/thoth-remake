14dece6bd8b1cc221faf6ac10166e9cab27edfbf
gpmatheus <gp.matheusluis@gmail.com>
2023-05-16 16:04:02 -0300

    fixed bug that displayed semicolon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
     <script src="assets/js/argon-dashboard.js"></script>
-    @stack('js');
+    @stack('js')
 </body>
 
 </html>
