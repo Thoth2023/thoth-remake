29a2d2d92f116433295ddbaf4361a1cbd5b12ca0
Auri Gabriel <ririmello.a@gmail.com>
2023-05-30 15:34:51 -0300

    feat: search strategy

---
+++ app/Http/Controllers/SearchStrategyController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\SearchStrategy;
+use App\Models\Project;
+
+class SearchStrategyController extends Controller
+{
+    public function index($projectId)
+    {
+        $project = Project::findOrFail($projectId);
+        $searchStrategy = $project->searchStrategy;
+        return view('planning.search_strategy', compact('project', 'searchStrategy'));
+    }
+
+    public function update(Request $request, $projectId)
+    {
+        $validatedData = $request->validate([
+            'planning.search_strategy' => 'required',
+        ]);
+
+        $project = Project::findOrFail($projectId);
+        $searchStrategy = $project->searchStrategy;
+
+        if ($searchStrategy) {
+            $searchStrategy->update([
+                'description' => $validatedData['search_strategy'],
+            ]);
+        } else {
+            SearchStrategy::create([
+                'description' => $validatedData['search_strategy'],
+                'project_id' => $project->id,
+            ]);
+        }
+
+        return redirect()->back()->with('success', 'Search strategy updated successfully.');
+    }
+}
