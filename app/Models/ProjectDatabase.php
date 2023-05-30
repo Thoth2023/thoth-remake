c0895e21ad9ddef9547d220ebb9eea3308d2010c
tobiastirola-unipampa <tobiastirola.aluno@unipampa.edu.br>
2023-05-30 17:47:48 -0300

    ProjectDatabase Model
    ProjectDatabase Model creation

---
+++ app/Models/ProjectDatabase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectDatabase extends Model
+{
+    protected $table = 'project_databases';
+
+    // since the primary key was not named as id, we need to specify it
+    // if the primary key was named as id, we would not need to specify it
+    // because laravel would automatically know that the primary key is id
+    // and would automatically set it
+    // TODO change the db to have the primary key named as id
+
+    protected $primaryKey = 'id_project_database';
+    public $timestamps = false;
+
+    use HasFactory;
+
+    protected $fillable = [
+        'id_project',
+        'id_database',
+    ];
+}
