c0470809cf0852aa1704d6e4a48be1fb0d4ea72a
DLMdm <dionasluanmuller3@gmail.com>
2023-05-31 01:23:43 -0300

    About page font correction

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
     @include('layouts.navbars.guest.navbar', ['title' => 'About'])
-    <div class="card d-inline-flex p-2 mt-8">
+    <div class="card d-inline-flex p-3 mt-8">
         <div class = "card-body">
-             <h2>Thoth 2.0</h2>
+            <a href="javascript:;" class="card-title h4 d-block text-darker">
+            Thoth 2.0
+            </a> 
             <div class = "card">
                 <div class = "card-body">
-                    <h4>About the Tool:</h4>
+                    <a href="javascript:;" class="card-title h5 d-block text-darker">
+                    About the Tool:
+                    </a> 
                     <p>Thoth is a powerful tool to support collaborative systematic reviews. Developed as a multi-platform solution, Thoth aims to automate important parts of the systematic review process, facilitating and streamlining the work of researchers and professionals involved in this type of study.</p>
                 </div>
             </div>
@@ -15,7 +19,9 @@
         <div class = "card-body">
             <div class = "card">
                 <div class = "card-body">
-                    <h4>New features</h4>
+                    <a href="javascript:;" class="card-title h5 d-block text-darker">
+                    New features:
+                    </a> 
                     <ul>
                         <li>Recover password</li>
                         <li>Bug Databases</li>
@@ -28,7 +34,9 @@
         <div class = "card-body">
             <div class = "card">
                 <div class = "card-body">
-                    <h4>Development</h4>
+                    <a href="javascript:;" class="card-title h5 d-block text-darker">
+                    Development
+                    </a> 
                     <p>Thoth was created by undergraduate students in Software Engineering, maintained and updated by students and professors of the Graduate Program in Software Engineering - PPGES at the Federal University of Pampa (UNIPAMPA).</p>
                 </div>
             </div>
@@ -36,12 +44,16 @@
         <div class = "card-body">
             <div class = "card">
                 <div class = "card-body">
-                    <h4>Open Source Project</h4>
-                    <a class="nav-link d-flex align-items-center me-2 active"
+                    <a href="javascript:;" class="card-title h5 d-block text-darker">
+                    Open Source Project
+                    </a> 
+                    <a class="nav-link d-flex align-items-center me-2"
                         href="https://github.com/Thoth2023/thoth2.0/blob/main/LICENSE">
                         MIT license
                     </a>
-                    <h6>Technologies used:</h6>
+                    <a href="javascript:;" class="card-title h6 d-block text-darker">
+                    Technologies used:
+                    </a> 
                     <ul>
                         <li >PHP Language</li>
                         <li >MySQL</li>
