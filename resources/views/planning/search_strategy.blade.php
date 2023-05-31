30357d0cf7fa26b0af107e766e9b179c2609d025
Auri Gabriel <ririmello.a@gmail.com>
2023-05-31 10:28:35 -0300

    feat: feedback message on the search strategy

--- resources/views/planning/search_strategy.blade.php
+++ resources/views/planning/search_strategy.blade.php
@@ -6,13 +6,20 @@
 <div class="card shadow-lg mx-4">
     <div class="container-fluid py-4">
         <p class="text-uppercase text-sm">Search Strategy</p>
-
+        @if(session()->has('message'))
+        <div class="alert alert-{{ session('message_type') }} alert-dismissible fade show" role="alert">
+            {{ session('message') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
         <form method="POST" action="{{ route('search-strategy.update', ['projectId' => $project->id_project]) }}">
             @csrf
             @method('post')
             <div class="form-group">
                 <label for="searchStrategyTextarea">Search Strategy</label>
-                <textarea name="search_strategy" class="form-control @error('search_strategy') is-invalid @enderror" id="searchStrategyTextarea" rows="8" placeholder="Enter the search strategy">{{ $project->searchStrategy->description ?? old('search_strategy') }}</textarea>
+                <textarea name="search_strategy" class="form-control @error('search_strategy') is-invalid @enderror" id="searchStrategyTextarea" rows="8" placeholder="Enter the search strategy">
+                {{ $project->searchStrategy->description ?? old('search_strategy') }}
+                </textarea>
 
                 @error('search_strategy')
                 <span class="invalid-feedback" role="alert">
@@ -22,7 +29,9 @@
             </div>
 
             <div class="d-flex align-items-center mt-4">
-                <button type="submit" class="btn btn-primary btn-sm ms-auto">Save</button>
+                <button type="submit" class="btn btn-primary btn-sm ms-auto">
+                    Save
+                </button>
                 <a onclick="modal_help('modal_help_strategy')" class="btn btn-secondary btn-sm me-2 opt">
                     <i class="fas fa-question-circle"></i> Help
                 </a>
