2eb552075127bbc2da83cb56b1382a01ad5a3023
Balda <mateusbalda89@gmail.com>
2023-06-05 05:25:39 -0300

    docs: methods used in controllerProject collaborators feature
    >
    >
    Co-authored-by: Joao Emilio Antonio Villa <jauzerv@outlook.com>

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -96,6 +96,13 @@ class ProjectController extends Controller
          return redirect('/projects');
     }
 
+    /**
+     * Remove a member from a project.
+     * @param string $idProject The ID of the project.
+     * @param mixed $idMember The ID of the member.
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
     public function destroy_member(string $idProject, $idMember)
     {
         $project = Project::findOrFail($idProject);
@@ -103,6 +110,12 @@ class ProjectController extends Controller
         return redirect()->back();
     }
 
+    /**
+     * Display the form to add a member to a project.
+     * @param string $idProject The ID of the project.
+     * @return \Illuminate\Contracts\View\View
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
     public function add_member(string $idProject) 
     {
         $project = Project::findOrFail($idProject); 
@@ -111,6 +124,13 @@ class ProjectController extends Controller
         return view('projects.add_member', compact('project','users_relation')); 
     }
     
+    /**
+     * Add a member to a project based on the submitted form data.
+     * @param \App\Http\Requests\ProjectAddMemberRequest $request The validated request object.
+     * @param string $idProject The ID of the project.
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
     public function add_member_project(ProjectAddMemberRequest $request, string $idProject)
     {   
         $request->validated();
@@ -125,6 +145,14 @@ class ProjectController extends Controller
         return redirect()->back();
     }
 
+    /**
+     * Update the level of a project member.
+     * @param \App\Http\Requests\UpdateMemberLevelRequest $request The validated request object.
+     * @param mixed $idProject The ID of the project.
+     * @param mixed $idMember The ID of the member.
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
     public function update_member_level(UpdateMemberLevelRequest $request, $idProject, $idMember)
     {
         $project = Project::findOrFail($idProject);
@@ -137,6 +165,12 @@ class ProjectController extends Controller
         return redirect()->back()->with('succes', 'The member level has been changed successfully.');
     }
 
+    /**
+     * Find the ID of a user based on their email.
+     * @param string $email The email of the user.
+     * @return mixed The ID of the user.
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
     public function findIdByEmail($email)
     {
         $user = User::where('email', $email)->firstOrFail();
