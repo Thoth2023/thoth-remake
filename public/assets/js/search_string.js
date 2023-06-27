a3d3fdb91e07a558b70e599eaa9ccb40446c4dec
Auri Gabriel <ririmello.a@gmail.com>
2023-06-27 09:27:01 -0300

    fix: related term growing outside container element

--- public/assets/js/search_string.js
+++ public/assets/js/search_string.js
@@ -7,8 +7,8 @@ async function related_terms(term){
 	for(let i = 0; i < final_terms.length; i++){
 		const divinp = document.createElement('div');
         divinp.className = 'input-group ';
-        divinp.className += 'col-md-1 '; 
-        divinp.className += 'offset-md-2 '; 
+        divinp.className += 'col-md-1 ';
+        // divinp.className += 'offset-md-2 ';
 
         const inp = document.createElement('input');
         inp.value = final_terms[i];
@@ -19,8 +19,8 @@ async function related_terms(term){
 
         const btn = document.createElement('button');
         btn.type = "button";
-        btn.className = "btn btn-primary"; 
-        btn.style.marginTop = "5px"; 
+        btn.className = "btn btn-primary";
+        btn.style.marginTop = "5px";
 
         btn.onclick = () => {
             add_synonym(final_terms[i]);
