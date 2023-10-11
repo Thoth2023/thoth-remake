8f7022db8ea2303d33b2132b18aa162c580fb4bb
Diego Comis <diegocomis@gmail.com>
2023-10-11 20:16:35 -0300

    - ajustes e limpeza de alguns arquivos não necessários

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
-
+<!-- PWA service worker -->
+<script>
+    if (typeof navigator.serviceWorker !== 'undefined') {
+        navigator.serviceWorker.register('pwabuilder-sw.js')
+    }
+</script>
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -21,3 +26,4 @@
     </div>
 </div>
 @endsection
+
