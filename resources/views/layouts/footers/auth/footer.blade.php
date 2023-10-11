c9fb022a8cf2ecb64fc2d732f354459c993bea9f
Diego Comis <diegocomis@gmail.com>
2023-10-11 19:57:35 -0300

    - ajustes e limpeza de alguns arquivos não necessários

--- resources/views/layouts/footers/auth/footer.blade.php
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -1,26 +1,30 @@
-<footer class="footer pt-3  ">
+<footer class="footer ">
     <div class="container-fluid">
-        <div class="row align-items-center justify-content-lg-between">
+        <div class="row align-items-center justify-content-lg-end">
             <div class="col-lg-6 mb-lg-0 mb-4">
                 <div class="copyright text-center text-sm text-muted text-lg-start">
                     ©
                     <script>
                         document.write(new Date().getFullYear())
                     </script>,
-                    Thoth :: Tool for RSL
+                    Thoth :: Tool for SLR
                 </div>
             </div>
             <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
-                        <a href="#" class="nav-link text-muted" target="_blank">FAQ</a>
+                        <a href="{{ route('help') }}" class="nav-link text-muted">FAQ</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link text-muted" target="_blank">About Us</a>
+                        <a href="{{ route('about') }}" class="nav-link text-muted">About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted"
-                            target="_blank">License</a>
+                        <a href="https://github.com/Thoth2023/thoth2.0" class="nav-link pe-0 text-muted"
+                            target="_blank">GitHub</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://www.lesse.com.br" class="nav-link pe-0 text-muted"
+                           target="_blank">Lesse</a>
                     </li>
                 </ul>
             </div>
