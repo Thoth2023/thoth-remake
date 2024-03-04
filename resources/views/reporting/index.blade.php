989d055387e93d792f8f6b92913a0c1e99bfd922
Auri Gabriel <ririmello.a@gmail.com>
2024-03-03 23:45:26 -0300

    feat(reporting): overview charts

--- resources/views/reporting/index.blade.php
+++ resources/views/reporting/index.blade.php
@@ -18,12 +18,10 @@
                             <a class="btn bg-gradient-faded-white mb-0"
                                 href="{{ route('projects.show', $project->id_project) }}">
                                 <i class="fas fa-plus"></i>Overview</a>
-
                         </li>
                         <li class="nav-item">
                             <a class="btn bg-gradient-faded-white mb-0"
                                 href="{{ route('planning.index', $project->id_project) }}">
-
                                 <i class="fas fa-plus"></i>Planning</a>
                         </li>
                         <li class="nav-item">
@@ -32,7 +30,6 @@
                         <li class="nav-item">
                             <a class="btn bg-gradient-dark mb-0"
                                 href="{{ route('reporting.index', $project->id_project) }}">
-
                                 <i class="fas fa-plus"></i>Reporting</a>
                         </li>
                         <li class="nav-item">
@@ -72,38 +69,137 @@
                             <a class="nav-link" id="data-extraction-tab" data-bs-toggle="tab"
                                 href="#data-extraction">Data Extraction</a>
                         </li>
-                        <!-- Add the rest of the tabs in a similar manner -->
                     </ul>
                 </div>
-
                 <div class="tab-content">
                     <div class="tab-pane fade show active" id="overview">
-                        </div>
-                        <div>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum
-                            voluptates. Quisquam, voluptatum voluptates. Quisquam, voluptatum voluptates.
-                            Quisquam, voluptatum voluptates. Quisquam, voluptatum voluptates. Quisquam,
-                        </div>
+                        <!-- Highcharts Chart -->
+                        <div id="overviewFunnelChart" style="height: 400px;" class="card my-2 p-2"></div>
+                        <div id="overviewChart" style="height: 400px;" class="card my-2 p-2"></div>
                     </div>
                     <div class="tab-pane fade" id="import-studies">
+                        <!-- Content for Import Studies tab -->
                     </div>
                     <div class="tab-pane fade" id="study-selection">
+                        <!-- Content for Study Selection tab -->
                     </div>
                     <div class="tab-pane fade" id="quality-assessment">
+                        <!-- Content for Quality Assessment tab -->
                     </div>
                     <div class="tab-pane fade" id="data-extraction">
+                        <!-- Content for Data Extraction tab -->
                     </div>
                 </div>
-
             </div>
-            @include('layouts.footers.auth.footer')
         </div>
+        @include('layouts.footers.auth.footer')
     </div>
 </div>
 @endsection
 
+@section('scripts')
+@parent
+@push('js')
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/funnel.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js"></script>
 <script>
+    // Function to store the active tab in a cookie
     function storeActiveTab(tabId) {
         document.cookie = "activeReportingTab=" + tabId + ";path=/";
     }
+
+    // Sample Highcharts chart
+    document.addEventListener('DOMContentLoaded', function () {
+
+        // Overview Tab
+
+        Highcharts.chart('overviewFunnelChart', {
+            chart: {
+                type: 'funnel'
+            },
+            title: {
+                text: '{{ $project->title }} Funnel'
+            },
+            plotOptions: {
+                series: {
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b> ({point.y:,.0f})',
+                        softConnector: true
+                    },
+                    center: ['40%', '50%'],
+                    neckWidth: '30%',
+                    neckHeight: '25%',
+                    width: '80%'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            series: [{
+                name: 'Unique users',
+                data: [
+                    ['Website visits', 15654],
+                    ['Downloads', 4064],
+                    ['Requested price list', 1987],
+                    ['Invoice sent', 976],
+                    ['Finalized', 846]
+                ]
+            }],
+
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        plotOptions: {
+                            series: {
+                                dataLabels: {
+                                    inside: true
+                                },
+                                center: ['50%', '50%'],
+                                width: '100%'
+                            }
+                        }
+                    }
+                }]
+            }
+        });
+
+        Highcharts.chart('overviewChart', {
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: 'Failure of Daily Project Activities'
+            },
+            xAxis: {
+                categories: ['Category 1', 'Category 2', 'Category 3']
+            },
+            yAxis: {
+                title: {
+                    text: 'Values'
+                }
+            },
+            series: [{
+                name: 'Series 1',
+                data: [10, 20, 30]
+            }]
+        });
+
+        // Import Studies Tab
+
+        // Study Selection Tab
+
+        // Quality Assessment Tab
+
+        // Data Extraction Tab
+
+    });
 </script>
+@endpush
+@endsection
