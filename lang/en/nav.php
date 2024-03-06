906d3ed0ad6a509c33b5d7ea1c2c1d76c6b7e0d0
Auri Gabriel <ririmello.a@gmail.com>
2024-03-06 03:49:09 -0300

    feat(i18n): translate guest navbar

---
+++ lang/en/nav.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for the navigation bar.
+    | 
+    |
+    */
+
+    'home' => 'Home',
+    'about' => 'About',
+    'help' => 'Help',
+    'sign_up' => 'Sign Up',
+    'sign_in' => 'Sign In',
+    'language' => 'Language',
+
+];
