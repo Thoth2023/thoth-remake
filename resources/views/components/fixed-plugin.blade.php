c4ba70b9b39aa0ab49bd1beaac4799495b2f499f
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2024-05-03 18:44:27 -0300

    refactor: remove fixed plugin floating button

--- resources/views/components/fixed-plugin.blade.php
+++ resources/views/components/fixed-plugin.blade.php
@@ -1,7 +1,4 @@
 <div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-        <i class="fa fa-cog py-2"> </i>
-    </a>
     <div class="card shadow-lg">
         <div class="card-header pb-0 pt-3 ">
             <div class="float-start">
