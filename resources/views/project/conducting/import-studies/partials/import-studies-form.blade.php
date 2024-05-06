5a98a94652f458d58724f532cfb6b7ede58704f7
Gabriele Silva Agostini <gabiagostini16@gmail.com>
2024-05-05 21:47:54 -0300

    feat: database

--- resources/views/project/conducting/import-studies/partials/import-studies-form.blade.php
+++ resources/views/project/conducting/import-studies/partials/import-studies-form.blade.php
@@ -1,22 +1,26 @@
 <div class="row">
     <div class="col-md-6">
         <div class="form-group">
-            <form role="form" method="POST"
-                action="{{ route('project.conducting-import-studies.imp.store', ['projectId' => $id_project]) }}"
-                enctype="multipart/form-data">
+            <form role="form" method="POST" action="{{ route('project.conducting-import-studies.imp.store', ['projectId' => $id_project]) }}" enctype="multipart/form-data">
                 @csrf
-                <label for="example-text-input"
-                    class="form-control-label">{{ __('project/conducting-import-studies.form.database') }}</label>
-                <select class="form-control" name="database">
-                    <option value="Google">{{ __('project/conducting-import-studies.form.google') }}</option>
-                </select>
-
-                <input class="form-control" type="hidden" name="id_project" value="{{ $id_project }}">
-                <input class="form-control" type="hidden" name="pre_selected" value="0">
-
-                <button type="submit"
-                    class="btn btn-success mt-3">{{ __('project/conducting-import-studies.form.add') }}</button>
+                <div class="form-group">
+                    <label for="database" class="form-control-label">{{ __('project/conducting-import-studies.form.database') }}</label>
+                    <select class="form-control" name="database">
+                        <option value="" disabled selected>{{ __('project/conducting-import-studies.form.select-database') }}</option>
+                        @forelse ($databases as $database)
+                            @if ($database->state == 'approved')
+                                <option value="{{ $database->id_database }}">{{ $database->name }}</option>
+                            @endif
+                        @empty
+                            <option disabled>{{ __('project/conducting-import-studies.form.no-databases') }}</option>
+                        @endforelse
+                    </select>
+                </div>
+                <input type="hidden" name="id_project" value="{{ $id_project }}">
+                <input type="hidden" name="pre_selected" value="0">
+                <button type="submit" class="btn btn-success mt-3">{{ __('project/conducting-import-studies.form.add') }}</button>
             </form>
         </div>
     </div>
 </div>
+
