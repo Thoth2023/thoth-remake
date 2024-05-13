19febe0f747dbfdcfbdba14bcf3ea43c1839e149
GianEsteves <115878514+GianEsteves@users.noreply.github.com>
2024-05-13 12:05:32 -0300

    validar dominio do email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'regex:/^[a-zA-Z0-9._%+-]+@(gmail|hotmail|yahoo)\.com$/i'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
