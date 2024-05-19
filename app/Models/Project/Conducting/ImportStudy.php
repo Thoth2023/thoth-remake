44daea701b32da9bdddc44dc0ac0e8cb51b0dca5
Gabriele Silva Agostini <gabiagostini16@gmail.com>
2024-05-18 21:13:17 -0300

    feat:models-conducting-import-study

---
+++ app/Models/Project/Conducting/ImportStudy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ImportStudy extends Model
+{
+    use HasFactory;
+
+    protected $table = 'importStudies';
+
+    protected $primaryKey = 'id_importStudy';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'id_project',
+        'id_database',
+        'file_name',
+        'description',
+        'imported_studies_count',
+        'failed_imports_count',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'id_project', 'id_project');
+    }
+
+    public function database()
+    {
+        return $this->belongsTo(Database::class, 'id_database', 'id_database');
+    }
+}
