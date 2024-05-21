36d1aaf35106c96d88b58b7b92c75b06b9970f0f
Tales Schifelbein Soares <talesschifelbeinsoares@gmail.com>
2024-05-21 17:22:06 -0300

    feat: :sparkles: Adicionada a internacionalização na página "Project > Overview".
    Adicionada a internacionalização no "Project > Overview", com isso foi necessário criar os arquivos para as "langs" em en e ptbr. Assim como atualizada a rota para que utilize o middleware de "Localization". Também modificado o arquivo "project-overview.blade.php" para que consiga mudar o idioma dos textos

---
+++ lang/pt_BR/project/overview.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'project' => 'Projeto',
+    'description' => 'Descrição',
+    'objectives' => 'Objetivos',
+    'members' => 'Membros',
+
+    'progress' => 'Progresso da Revisão Sistemática',
+    'planning' => 'Planejamento',
+    'import_studies' => 'Importar Estudos',
+    'study_selection' => 'Seleção de Estudos',
+    'quality_assessment' => 'Avaliação de Qualidade',
+    'data_extraction' => 'Extração de Dados',
+
+    'activity_record' => 'Registro de Atividades',
+];
