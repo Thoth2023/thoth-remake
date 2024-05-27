726c44922be4be20cb7371d89cfff1f3903d0f0d
Tales Schifelbein Soares <talesschifelbeinsoares@gmail.com>
2024-05-27 17:21:15 -0300

    feat: :sparkles: Adicionada a internacionalização na Sidenav (esquerda)
    Adicionada a internacionalização na "Sidenav", com isso foi necessário criar os arquivos para as "langs" em en e ptbr. Também modificado o arquivo "fixed-plugin.blade.php" para que consiga mudar o idioma dos textos
    
    #251

--- lang/en/nav/side.php
+++ lang/en/nav/side.php
@@ -5,5 +5,25 @@ return [
     'my_projects' => 'My Projects',
     'profile' => 'Profile',
     'about_us' => 'About Us',
-    'help' => 'Help'
+    'help' => 'Help',
+    
+    // Left side navbar
+
+    'thoth_configurator' => 'Thoth Configurator',
+    'dashboard_options' => 'Dashboard Options',
+
+    'language_selection' => 'Language Selection',
+
+    'sidebar_color' => 'Sidebar Color',
+
+    'sidenav_type' => 'Sidenav Type',
+    'sidenav_choose' => 'Choose between 2 different sidenav types.',
+
+    'white' => 'White',
+    'dark' => 'Dark',
+
+    'sidenav_warning' => 'You can change the sidenav type just on desktop view.',
+
+    'navbar_fixed' => 'Navbar Fixed',
+    'light_dark' => 'Light/Dark',
 ];
--- lang/pt_BR/nav/side.php
+++ lang/pt_BR/nav/side.php
@@ -5,5 +5,25 @@ return [
     'my_projects' => 'Meus projetos',
     'profile' => 'Perfil',
     'about_us' => 'Sobre nós',
-    'help' => 'Ajuda'
+    'help' => 'Ajuda',
+
+    // Left side navbar
+
+    'thoth_configurator' => 'Configuração do Thoth',
+    'dashboard_options' => 'Opções do painel',
+
+    'language_selection' => 'Seleção de idioma',
+
+    'sidebar_color' => 'Cor da barra lateral',
+
+    'sidenav_type' => 'Tipo de barra lateral',
+    'sidenav_choose' => 'Escolha entre 2 tipos de barras laterais diferentes.',
+
+    'white' => 'Branco',
+    'dark' => 'Escuro',
+
+    'sidenav_warning' => 'Você pode alterar o tipo de barra lateral apenas na visualização de desktop.',
+
+    'navbar_fixed' => 'Barra de navegação fixa',
+    'light_dark' => 'Claro/Escuro',
 ];
