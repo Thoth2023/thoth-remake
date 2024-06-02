52b47475cece92444219a6cbc89d9a60ba3bf6e5
GABRIELE_SA\bruxa <gabiagostini16@gmail.com>
2024-06-02 20:07:01 -0300

    feat: tradução-ptbr

--- lang/en/project/conducting.php
+++ lang/en/project/conducting.php
@@ -2,13 +2,10 @@
 
 return [
 
-    'conducting' => 'Conducting',
-    'header' => [
-        'overview' => 'Overview',
-        'import_studies' => 'Import Studies',
-        'study_selection' => 'Study Selection',
-        'quality_assessment' => 'Quality Assessment',
-        'data_extraction' => 'Data Extraction',
-    ]
-
+        'import-studies' => [
+             'form' => [
+                'add' =>  'Adicionar alguma coisa',
+                'delete' => 'Deletar'
+             ]
+        ]
 ];
--- lang/pt_BR/project/conducting.php
+++ lang/pt_BR/project/conducting.php
@@ -1,15 +1,12 @@
-
 <?php
 
 return [
 
-    'conducting' => 'Condução',
-    'header' => [
-        'overview' => 'Visão Geral',
-        'import_studies' => 'Importar Estudos',
-        'study_selection' => 'Seleção de Estudos',
-        'quality_assessment' => 'Avaliação de Qualidade',
-        'data_extraction' => 'Extração de Dados',
-    ]
-
+        'import-studies' => [
+            'title' => 'Import',
+             'form' => [
+                'add' =>  'Adicionar alguma coisa',
+                'delete' => 'Deletar'
+             ]
+        ]
 ];
