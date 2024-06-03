2245c4708d0e0ea330c54d2db54c8de756b6c9bb
KauaHenrico <108253678+KauaHenrico@users.noreply.github.com>
2024-06-03 15:49:56 -0300

    copyright in show.blade

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -20,6 +20,6 @@
                     </div>
                 </div>
             </div>
-            <!-- @include("layouts.footers.auth.footer") -->
+            @include("layouts.footers.auth.footer")
         </div>
     @endsection
