9112c037898299c550e19a6ff6497867528ca8e6
Tales Schifelbein Soares <talesschifelbeinsoares@gmail.com>
2024-06-06 22:19:09 -0300

    feat: :sparkles: V1 da tela de Termos e Condições.
    V1 da tela de Termos e Condições. Criado o "TermsController" para poder exibir a página. Criado o "terms.blade.php" que é a página em si. Criado os arquivos lang, para a internacionalizção da página. Alterado o arquivo "web.php" para incluir a rota da nova página. Alterado o arquivo "register.blade.php" que é na tela onde o link para a página de Termos aparece.
    
    265

---
+++ resources/views/pages/terms.blade.php
@@ -0,0 +1,112 @@
+@extends("layouts.app")
+
+@section("content")
+@include("layouts.navbars.guest.navbar", ["title" => "Home"])
+
+<div class="container mt-8 mb-3">
+    <div class="page-header d-flex flex-column pt-4 pb-11 border-radius-lg">
+        <div class="row justify-content-center rounded-3 py-4" style="background-color: rgba(85, 101, 128, 1); width: 100%">
+            <div class="col-lg-6 text-center mx-auto">
+                <h1 class="text-white">
+                    {{ __("pages/terms.terms") }}
+                </h1>
+                <p class="text-lead text-white">
+                    <!-- {{ __("pages/terms.what") }} -->
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="mt-lg-n12 mt-md-n13 mt-n12 justify-content-center">
+            <div class="card d-inline-flex p-3 mt-5">
+                <div class="card-body pt-2">
+                    <a href="javascript:;" class="card-title h4 d-block text-darker">
+                        {{ __("pages/terms.what") }}
+                    </a>
+                    <p>
+                        {{ __("pages/terms.what_text") }}
+                        <a href="https://thoth-rsl.com">https://thoth-rsl.com</a>
+                    </p>
+
+                    <p>{{ __("pages/terms.objective") }}</p>
+                    <p>{{ __("pages/terms.author") }}</p>
+                </div>
+                <div class="card-body pt-2">
+                    <a href="javascript:" class="card-title h4 d-block text-darker">
+                        {{ __("pages/terms.authoral") }}
+                    </a>
+                    <p>{{ __("pages/terms.authoral_text") }}</p>
+                </div>
+                <div class="card-body pt-2">
+                    <a href="javascript:" class="card-title h4 d-block text-darker">
+                        {{ __("pages/terms.who") }}
+                    </a>
+                    <p>{{ __("pages/terms.who_text") }}</p>
+                </div>
+                <div class="card-body pt-2">
+                    <a href="javascript:" class="card-title h4 d-block text-darker">
+                        {{ __("pages/terms.commitment") }}
+                    </a>
+                    <p>{{ __("pages/terms.commitment_text") }}</p>
+                    <ul>
+                        <li>
+                            <a class="card-title h6 d-block text-darker">
+                                {{ __("pages/terms.commitment_1") }}
+                            </a>
+                        </li>
+                        <li>
+                            <a class="card-title h6 d-block text-darker">
+                                {{ __("pages/terms.commitment_2") }}
+                            </a>
+                        </li>
+                        <li>
+                            <a class="card-title
+                            h6 d-block text-darker">
+                                {{ __("pages/terms.commitment_3") }}
+                            </a>
+                        </li>
+                        <li>
+                            <a class="card-title h6 d-block text-darker">
+                                {{ __("pages/terms.commitment_4") }}
+                            </a>
+                        </li>
+                        <li>
+                            <a class="card-title h6 d-block text-darker">
+                                {{ __("pages/terms.commitment_5") }}
+                            </a>
+                        </li>
+                        <li>
+                            <a class="card-title h6 d-block text-darker">
+                                {{ __("pages/terms.commitment_6") }}
+                            </a>
+                        </li>
+                        <li>
+                            <a class="card-title h6 d-block text-darker">
+                                {{ __("pages/terms.commitment_7") }}
+                            </a>
+                        </li>
+                        <li>
+                            <a class="card-title h6 d-block text-darker">
+                                {{ __("pages/terms.commitment_8") }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body pt-2">
+                    <a href="javascript:" class="card-title h4 d-block text-darker">
+                        {{ __("pages/terms.responsability") }}
+                    </a>
+                    <p>{{ __("pages/terms.responsability_text") }}</p>
+                </div>
+                <div class="card-body pt-2">
+                    <a href="javascript:" class="card-title h4 d-block text-darker">
+                        {{ __("pages/terms.privacy") }}
+                    </a>
+                    <p>{{ __("pages/terms.privacy_text") }}</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
