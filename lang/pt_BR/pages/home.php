8721f14fb9b9836a36c49528dea266a275736fd1
Ricardo Costa <ricardo_hentges@hotmail.com>
2024-06-09 18:48:32 -0300

    refactor: :recycle: Alterada a descrição do home
    Alterada a descrição do home para algo mais contextual
    
    #276

--- lang/pt_BR/pages/home.php
+++ lang/pt_BR/pages/home.php
@@ -3,7 +3,7 @@
 return [
     'home' => 'Página Inicial',
     'welcome' => 'Bem-vindo!',
-    'project_description' => 'Use estes formulários incríveis para fazer login ou criar uma nova conta no seu projeto gratuitamente.',
+    'project_description' => 'Thoth foi criado por estudantes de graduação em Engenharia de Software, mantido e atualizado por alunos e professores do Programa de Pós-Graduação em Engenharia de Software - PPGES da Universidade Federal do Pampa (UNIPAMPA).',
     'thoth' => 'Thoth',
     'thoth_description' => 'É uma ferramenta poderosa para apoiar revisões sistemáticas colaborativas. É uma solução multiplataforma desenvolvida para automatizar partes importantes do processo de revisão sistemática, facilitando e agilizando o trabalho de pesquisadores e profissionais envolvidos neste tipo de estudo.',
     'questions' => 'Perguntas',
