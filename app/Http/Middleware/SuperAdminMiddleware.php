9d4e166349383983ad3488f289679e32e2f9b27d
Sabrina Rodrigues Fernandes <sabrinarfernandes.srf@gmail.com>
2024-06-10 18:08:36 -0300

    :construction: feat: Página de login super admin

--- app/Http/Middleware/SuperAdminMiddleware.php
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class SuperAdminMiddleware
@@ -15,7 +16,7 @@ class SuperAdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->role == 'superadmin') {
+        if (Auth::check() && Auth::user()->access_level == 3) {
             return $next($request);
         }
         return redirect('/'); 
