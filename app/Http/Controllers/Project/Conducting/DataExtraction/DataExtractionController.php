b01baed386940a0906be030c0756c227fd1652e1
FCardozera <felipebcardoso0711@outlook.com>
2024-06-10 12:59:53 -0300

    refactor: :art: Criado o controller para o Conducting -> Data Extraction
    Apenas criado o controller

---
+++ app/Http/Controllers/Project/Conducting/DataExtraction/DataExtractionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Project\Conducting;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class DataExtractionController extends Controller
+{
+
+    public function index(string $id_project) {
+
+        $project = Project::findOrFail($id_project);
+        return view('project.conducting.data-extraction', compact('project'));
+    }
+
+    
+}
