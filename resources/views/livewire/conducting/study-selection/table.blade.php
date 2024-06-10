778f1198fb556fbc1381a794292cf20150b0e41d
Marcos Silva <marcoseduardo_lopes_@hotmail.com>
2024-06-10 15:03:48 -0300

    feat: Improve search bar layout in study selection page

--- resources/views/livewire/conducting/study-selection/table.blade.php
+++ resources/views/livewire/conducting/study-selection/table.blade.php
@@ -4,80 +4,83 @@
     </div>
     @livewire("conducting.study-selection.search")
     <ul class='list-group'>
-        <li class='list-group-item'>
-            <div class='row'>
-                <div class='col-1'>
-                    <b wire:click="sortBy('id')" role="button" href="#">
-                        {{ __('project/conducting.study-selection.table.id' )}}
-                        @if(isset($sorts['id']))
-                            @if($sorts['id'] === 'asc')
-                                ↑
-                            @else
-                                ↓
-                            @endif
+        <li class='list-group-item d-flex'>    
+            <div class='w-5 pl-2'>
+                <b wire:click="sortBy('id')" role="button" href="#">
+                    {{ __('project/conducting.study-selection.table.id' )}}
+                    @if(isset($sorts['id']))
+                        @if($sorts['id'] === 'asc')
+                            ↑
+                        @else
+                            ↓
                         @endif
-                    </b>
-                </div>
-                <div class='col'>
-                    <b wire:click="sortBy('title')" role="button" href="#">
-                        {{ __('project/conducting.study-selection.table.title' )}}
-                        @if(isset($sorts['title']))
-                            @if($sorts['title'] === 'asc')
-                                ↑
-                            @else
-                                ↓
-                            @endif
+                    @endif
+                </b>
+            </div>
+            <div class='w-20 pl-2 pr-2'>
+                <b wire:click="sortBy('title')" role="button" href="#">
+                    {{ __('project/conducting.study-selection.table.title' )}}
+                    @if(isset($sorts['title']))
+                        @if($sorts['title'] === 'asc')
+                            ↑
+                        @else
+                            ↓
                         @endif
-                    </b>
-                </div>
-                <div class='col'>
-                    <b wire:click="sortBy('ac')" role="button" href="#">
-                        {{ __('project/conducting.study-selection.table.acceptance-criteria' )}}
-                        @if(isset($sorts['ac']))
-                            @if($sorts['ac'] === 'asc')
-                                ↑
-                            @else
-                                ↓
-                            @endif
+                    @endif
+                </b>
+            </div>
+            <div class='w-20'>
+                <b wire:click="sortBy('ac')" role="button" href="#">
+                    {{ __('project/conducting.study-selection.table.acceptance-criteria' )}}
+                    @if(isset($sorts['ac']))
+                        @if($sorts['ac'] === 'asc')
+                            ↑
+                        @else
+                            ↓
                         @endif
-                    </b>
-                </div>
-                <div class='col'>
-                    <b wire:click="sortBy('ec')" role="button" href="#">
-                        {{ __('project/conducting.study-selection.table.rejection-criteria' )}}
-                        @if(isset($sorts['ec']))
-                            @if($sorts['ec'] === 'asc')
-                                ↑
-                            @else
-                                ↓
-                            @endif
+                    @endif
+                </b>
+            </div>
+            <div class='w-20 pl-2 pr-2'>
+                <b wire:click="sortBy('ec')" role="button" href="#">
+                    {{ __('project/conducting.study-selection.table.rejection-criteria' )}}
+                    @if(isset($sorts['ec']))
+                        @if($sorts['ec'] === 'asc')
+                            ↑
+                        @else
+                            ↓
                         @endif
-                    </b>
-                </div>
-                <div class='col-2'>
-                    <b wire:click="sortBy('database')" role="button" href="#">
-                        {{ __('project/conducting.study-selection.table.database') }}
-                        @if(isset($sorts['database']))
-                            @if($sorts['database'] === 'asc')
-                                ↑
-                            @else
-                                ↓
-                            @endif
+                    @endif
+                </b>
+            </div>
+            <div class='w-15 pl-2 pr-2'>
+                <b wire:click="sortBy('database')" role="button" href="#">
+                    {{ __('project/conducting.study-selection.table.database') }}
+                    @if(isset($sorts['database']))
+                        @if($sorts['database'] === 'asc')
+                            ↑
+                        @else
+                            ↓
                         @endif
-                    </b>
-                </div>
-                <div class='col-1'>
-                    <b wire:click="sortBy('status')" role="button" href="#">
-                        {{ __('project/conducting.study-selection.table.status') }}
-                        @if(isset($sorts['status']))
-                            @if($sorts['status'] === 'asc')
-                                ↑
-                            @else
-                                ↓
-                            @endif
+                    @endif
+                </b>
+            </div>
+            <div class='pr-5 w-15'>
+                <b wire:click="sortBy('status')" role="button" href="#">
+                    {{ __('project/conducting.study-selection.table.status') }}
+                    @if(isset($sorts['status']))
+                        @if($sorts['status'] === 'asc')
+                            ↑
+                        @else
+                            ↓
                         @endif
-                    </b>
-                </div>
+                    @endif
+                </b>
+            </div>
+            <div class='w-5'>
+                <b>
+                    {{ __('project/conducting.study-selection.table.actions') }}
+                </b>
             </div>
         </li>
     </ul>
@@ -86,24 +89,24 @@
             <x-search.item
                 wire:key="{{ $paper['id_paper'], $paper['title'], $paper['data_base'], $paper['status']}}"
                 target="search-papers"
-                class="list-group-item d-flex row"
+                class="list-group-item d-flex row w-100"
             >
-                <div class='w-3'>
+                <div class='w-5 pl-2'>
                     <b>{{ $paper['id_paper'] }}</b>
                 </div>
-                <div class='w-5'>
+                <div class='w-20'>
                     <b>{{ $paper['title'] }}</b>
                 </div>
-                <div class='w-5'>
+                <div class='w-20'>
                     <b>[wip]</b>
                 </div>  
-                <div class='w-5'>
+                <div class='w-20'>
                     <b>[wip]</b>
                 </div>
-                <div class='w-5'>
+                <div class='w-15'>
                     <b>{{ $paper['data_base'] }}</b>
                 </div>
-                <div class=''>
+                <div class='w-15'>
                     @if(isset($editingStatus[$paper->id]))
                         <select wire:model="editingStatus.{{ $paper->id }}" wire:change="updateStatus({{ $paper->id }}, $event.target.value)">
                             @foreach($statuses as $status)
@@ -111,10 +114,13 @@
                             @endforeach
                         </select>
                     @else
-                        <option wire:click="$set('editingStatus.{{ $paper->id }}', '{{ $paper->status }}')">
+                        <b wire:click="$set('editingStatus.{{ $paper->id }}', '{{ $paper->status }}')">
                             {{ $paper->status }}
-                        </option>
+                        </b>
                     @endif
+                </div>
+                <div class='btn btn-light w-5'>
+                    <i class="fa-solid fa-share"></i>
                 </div>
             </x-search.item>
             @empty
