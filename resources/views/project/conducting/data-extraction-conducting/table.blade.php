1e25bdf119359969aa89e5df20679191cacd43dd
Joao Medina <joaomedinap@gmail.com>
2024-06-10 13:31:41 -0300

    refactor: :zap: Separação do html em componentes livewire
    Criação dos componentes tela Conducting > Data Extraction

---
+++ resources/views/project/conducting/data-extraction-conducting/table.blade.php
@@ -0,0 +1,18 @@
+<div class="card mb-0 mt-5">
+    <div class="container-fluid py-4">
+        <div class="table-responsive">
+            <table class="table align-items-center justify-content-center mb-0">
+                <thead>
+                    <tr>
+                        <th class="text-uppercase text-secondary">{{ __('project/conducting.table.id') }}</th>
+                        <th class="text-uppercase text-secondary">{{ __('project/conducting.table.title') }}</th>
+                        <th class="text-uppercase text-secondary">{{ __('project/conducting.table.author') }}</th>
+                        <th class="text-uppercase text-secondary">{{ __('project/conducting.table.year') }}</th>
+                        <th class="text-uppercase text-secondary">{{ __('project/conducting.table.data_base') }}</th>
+                        <th class="text-uppercase text-secondary">{{ __('project/conducting.table.status') }}</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
