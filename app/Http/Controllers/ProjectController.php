fb366912c311a581d930a6a01d233a9126f5431f
FCardozera <felipebcardoso0711@outlook.com>
2024-06-10 13:36:47 -0300

    feat: :bug: Implementada a função de Copy Planning para a edição de projeto
    Commit Changelog:  - FIX: Front-end do Copy Planning na página de edição de projeto, puxando os projetos do usuário; - FIX: Ajustada a tradução da página de edição do projeto; - FEATURE: Implementada a ação de copiar planejamento - Copy Planning, para a página de edição do projeto;

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -97,6 +97,7 @@ class ProjectController extends Controller
      */
     public function edit(string $idProject)
     {
+        $userProjects = Auth::user()->projects;
         $project = Project::findOrFail($idProject);
         $user = Auth::user();
 
@@ -119,7 +120,17 @@ class ProjectController extends Controller
             return redirect()->back()->with('error', 'You do not have permission to edit the project.');
         }
 
-        $project->update($request->all());
+        $project->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'objectives' => $request->objectives,
+        ]);
+
+        if ($request->copy_planning !== 'none') {
+            $sourceProject = Project::findOrFail($request->copy_planning);
+            $project->copyPlanningFrom($sourceProject);
+        }
+
         $activity = "Edited project";
         ActivityLogHelper::insertActivityLog($activity, 1, $project->id_project, $user->id);
 
