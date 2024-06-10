c774b3629657b850af2a13ed71c87efad4d497ae
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2024-06-10 15:32:11 -0300

    feat(accoridon-button): add minus button on toggle

--- resources/views/components/table/accordion-button.blade.php
+++ resources/views/components/table/accordion-button.blade.php
@@ -18,8 +18,12 @@
                 content.style.display === ''
             ) {
                 content.style.display = 'table-row';
+                button.querySelector('i').classList.remove('fa-plus');
+                button.querySelector('i').classList.add('fa-minus');
             } else {
                 content.style.display = 'none';
+                button.querySelector('i').classList.remove('fa-minus');
+                button.querySelector('i').classList.add('fa-plus');
             }
         }
     </script>
