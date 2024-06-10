007b513dc1e366b7933a651496a82ae9948d707e
Joao Medina <joaomedinap@gmail.com>
2024-06-10 14:58:31 -0300

    feat: :art: Filtro ordem crescente e decrescente
    Adesão de um script para filtrar a ordem dos estudos na tabela Conducting > Data Extraction

--- resources/views/project/conducting/data-extraction-conducting/table.blade.php
+++ resources/views/project/conducting/data-extraction-conducting/table.blade.php
@@ -1,7 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
+    <style>
+        .table-wrapper {
+            overflow-x: auto;
+        }
+    </style>
+</head>
+<body>
+
 <div class="card mb-0 mt-5">
     <div class="container-fluid py-4">
         <div class="table-responsive">
-            <table class="table align-items-center justify-content-center mb-0">
+            <table class="table align-items-center justify-content-center mb-0" id="study-selection-table">
                 <thead>
                     <tr>
                         <th class="text-uppercase text-secondary">{{ __('project/conducting.table.id') }}</th>
@@ -12,7 +26,26 @@
                         <th class="text-uppercase text-secondary">{{ __('project/conducting.table.status') }}</th>
                     </tr>
                 </thead>
+                <!-- Your table body here -->
             </table>
         </div>
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#study-selection-table').DataTable({
+        "searching": false,
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.25/i18n/Portuguese-Brasil.json"
+        }
+    });
+});
+</script>
+
+</body>
+</html>
