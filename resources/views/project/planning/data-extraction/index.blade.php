4b1fc87124a16d50c871e7dece51b05a95aaedda
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2024-06-17 08:47:10 -0300

    fix: minor changes

--- resources/views/project/planning/data-extraction/index.blade.php
+++ resources/views/project/planning/data-extraction/index.blade.php
@@ -1,4 +1,4 @@
-<div class="col-12 mb-3">
+<div class="col-12 m-0 mb-3">
     <div class="grid-items-2 gap-4">
         @livewire("planning.data-extraction.question")
         @livewire("planning.data-extraction.option")
