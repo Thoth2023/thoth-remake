1d8e8c00aecb1d0ae75e8eefa6c39a7abd82145a
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2024-06-17 17:16:58 -0300

    fix?: init qa table

--- resources/views/project/planning/quality-assessment.blade.php
+++ resources/views/project/planning/quality-assessment.blade.php
@@ -3,6 +3,7 @@
         {{-- Question Quality --}}
         @livewire("planning.quality-assessment.question-quality")
         @livewire("planning.quality-assessment.question-score")
+        @livewire("planning.quality-assessment.question-qa-table")
     </div>
     <div class="grid-items-1 gap-4 my-4">[wip]</div>
 </div>
