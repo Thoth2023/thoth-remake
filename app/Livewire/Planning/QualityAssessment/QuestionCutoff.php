8a0f9a8698e6d386a100ca6e0b456d741ca44e07
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2024-06-17 17:16:24 -0300

    refactor?: init livewire attributes

--- app/Livewire/Planning/QualityAssessment/QuestionCutoff.php
+++ app/Livewire/Planning/QualityAssessment/QuestionCutoff.php
@@ -6,6 +6,16 @@ use Livewire\Component;
 
 class QuestionCutoff extends Component
 {
+  public $sum = 0;
+  public $cutoff = 0;
+  public $questions = [];
+
+  public function mount()
+  {
+    $this->sum = 0;
+    $this->cutoff = 0;
+  }
+
   public function render()
   {
     return view('livewire.planning.quality-assessment.question-cutoff');
