eaee797837a3c1d2cd3cee810e48cf508f0b8572
FCardozera <felipebcardoso0711@outlook.com>
2024-06-16 22:17:38 -0300

    fix: :bug: Correção do bug que não estava deixando criar um projeto com a função de copiar planejamento
    #224

--- app/Http/Requests/Project/ProjectStoreRequest.php
+++ app/Http/Requests/Project/ProjectStoreRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Project;
 
+use App\Models\Project;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ProjectStoreRequest extends FormRequest
@@ -26,10 +27,8 @@ class ProjectStoreRequest extends FormRequest
             'description' => 'required|string',
             'objectives' => 'required|string',
             'copy_planning' => ['nullable', 'string', function ($attribute, $value, $fail) {
-                if ($value !== 'none') {
-                    if (!Project::where('id_project', $value)->exists()) {
-                        $fail('The selected :attribute is invalid.');
-                    }
+                if ($value !== 'none' && !(Project::where('id_project', $value)->exists())) {
+                    $fail('The selected :attribute is invalid.');
                 }
             }],
         ];
