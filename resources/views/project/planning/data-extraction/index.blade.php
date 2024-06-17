c3b40c9c274268c74b0d1a6eda3b2b67bc09ca0c
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2024-06-17 08:37:26 -0300

    fix: livewire data extraction null array

--- resources/views/project/planning/data-extraction/index.blade.php
+++ resources/views/project/planning/data-extraction/index.blade.php
@@ -1,9 +1,9 @@
 <div class="col-12 mb-3">
     <div class="grid-items-2 gap-4">
         @livewire("planning.data-extraction.question")
-        @livewire("planning.data-extraction.option")
+        {{-- @livewire("planning.data-extraction.option") --}}
     </div>
     <div class="grid-items-1">
-        @livewire("planning.data-extraction.data-extraction")
+        {{-- @livewire("planning.data-extraction.data-extraction") --}}
     </div>
 </div>
