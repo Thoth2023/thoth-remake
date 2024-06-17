3269f84e5bdfa6f553667011e690b486d5cc89f0
Ricardo Costa <ricardo_hentges@hotmail.com>
2024-06-17 15:05:16 -0300

    feat: :globe_with_meridians: Adição da i18n no título de "Meus Projetos" e "Criar Projeto"
    Adição da i18n no título de "Meus Projetos" e "Criar Projeto", ambos os arquivos de tradução das páginas estavam criados, apenas faltava inseri-los no blade

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,24 +1,30 @@
 @extends("layouts.app", ["class" => "g-sidenav-show bg-gray-100"])
 
 @section("content")
-    @include("layouts.navbars.auth.topnav", ["title" => __("nav/topnav.projects")])
+    @include("layouts.navbars.auth.topnav", ["title" => __("project/projects.project.title") ])
 
-    <div class="container-fluid-project py-2">
-        
+    <div class="container-fluid py-2">
+        <div class="container-fluid py-2">
             <div class="row">
                 <div class="col-12">
                     <div class="card mb-4">
-                         <div class="card-header pb-0">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <h6 class="mb-0">
-                                    <i class="ni ni-single-copy-04 text-primary text-sm opacity-10"></i>
-                                    {{ __("project/projects.project.table.title") }}
-                                </h6>
-                                <a class="btn bg-gradient-dark mb-0" href="{{ route('projects.create') }}">
+                        <div class="card-header pb-0">
+                            <div class="col-12 text-end">
+                                <a
+                                    class="btn bg-gradient-dark mb-0"
+                                    href="{{ route("projects.create") }}"
+                                >
                                     <i class="fas fa-plus"></i>
                                     &nbsp;&nbsp;{{ __("project/projects.project.new") }}
                                 </a>
                             </div>
+
+                            <h6>
+                                <i
+                                    class="ni ni-single-copy-04 text-primary text-sm opacity-10"
+                                ></i>
+                                {{ __("project/projects.project.table.title") }}
+                            </h6>
                         </div>
                         <div class="card-body px-0 pt-0 pb-2">
                             <div class="table-responsive p-0">
@@ -181,6 +187,7 @@
                     </div>
                 </div>
             </div>
+        </div>
         @include("layouts.footers.auth.footer")
     </div>
 @endsection
