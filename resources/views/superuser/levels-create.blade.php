1f69dfdf57bed1391c9e5434a632c234a8b26fb7
bianca <biancamr.aluno@unipampa.edu.br>
2024-06-21 23:16:04 -0300

    adicionar novo grupo de perfil

--- resources/views/superuser/levels-create.blade.php
+++ resources/views/superuser/levels-create.blade.php
@@ -1,71 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.navbars.auth.topnav', ['title' => __('superuser/profile.your_profile')])
+@include('layouts.navbars.auth.topnav', ['title' => 'Adicionar Novo Perfil de Usuário'])
 
-<div class="container mt-8 mb-3">
-
-    <div class="card shadow-lg ">
-        <div class="card-body">
-            <div class="row gx-4">
-                <div class="col-auto my-auto">
-                    <div class="h-100">
-                        <h5 class="mb-1">
-                            {{ auth()->user()->firstname ? auth()->user()->firstname : auth()->user()->username }}
-                            {{ auth()->user()->lastname ? auth()->user()->lastname : '' }}
-                        </h5>
-                        <p class="mb-0 font-weight-bold text-sm">
-                        Cadastrar novo grupo {{  (auth()->user()->occupation ? auth()->user()->occupation : '') }}
-                        </p>
-
-                    </div>
+<div class="row mt-4 mx-4">
+    <div class="col-12">
+        <div class="card mb-4">
+            <div class="card-header pb-0">
+                <h6>Adicionar Novo Perfil de Usuário</h6>
+            </div>
+            <div class="card-body px-0 pt-0 pb-2">
+                <div class="p-3">
+                    <form action="{{ route('levels.store') }}" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="level" class="form-label">Nome do Perfil</label>
+                            <input type="text" class="form-control" id="level" name="level" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Descrição</label>
+                            <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success">Salvar</button>
+                        <a href="{{ route('levels.index') }}" class="btn btn-secondary">Cancelar</a>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-
-    <br>
-    <div id="alert">
-        @include('components.alert')
-    </div>
-    <div class="row" style="justify-content: space-around;">
-        <div class="col-md-12">
-            <div class="card">
-                <form role="form" method="POST" action="{{ route('level.store') }}" enctype="multipart/form-data">
-                @csrf
-                                <div class="flex flex-col mb-3 col-xl-6 col-lg-5 col-md-6 mx-auto">
-                                    <input type="text" name="username" class="form-control" style="margin-top: 3rem;"
-                                        placeholder="{{ __('auth/register.username') }}"
-                                        aria-label="{{ __('auth.register.username') }}" value="{{ old('username') }}">
-                                    @error('username')
-                                        <p class='text-danger text-xs pt-1'> {{ $message }} </p>
-                                    @enderror
-                                </div>
-                                <div class="flex flex-col mb-3 col-xl-6 col-lg-5 col-md-6 mx-auto">
-                                    <input type="email" name="email" class="form-control"
-                                        placeholder="{{ __('auth/register.email') }}"
-                                        aria-label="{{ __('auth.register.email') }}" value="{{ old('email') }}">
-                                    @error('email')
-                                        <p class='text-danger text-xs pt-1'> {{ $message }} </p>
-                                    @enderror
-                                </div>
-                                <div class="flex flex-col mb-3 col-xl-6 col-lg-5 col-md-6 mx-auto">
-                                    <input type="password" name="password" class="form-control"
-                                        placeholder="{{ __('auth/register.password') }}"
-                                        aria-label="{{ __('auth.register.password') }}">
-                                    @error('password')
-                                        <p class='text-danger text-xs pt-1'> {{ $message }} </p>
-                                    @enderror
-                                </div>
-                                <div class="text-center">
-                                    <button type="submit"
-                                        class="btn bg-gradient-dark w-50 my-4 mb-2 ">{{ __('auth/register.sign_up') }}</button>
-                                </div>
-                                <br/><br/>
-                </form>
-            </div>
-        </div>
-    </div>
-    @include('layouts.footers.auth.footer')
 </div>
 @endsection
