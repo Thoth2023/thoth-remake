84b3362fc6b1c475711a1122224a9215130ea264
GABRIELE_SA\bruxa <gabiagostini16@gmail.com>
2024-06-23 19:56:22 -0300

    feat: routes

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,6 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Planning\Databases\Databases;
 use App\Http\Controllers\Project\ConductingController;
 
-use Livewire\Planning\Databases\Databases;
 use App\Http\Controllers\ThemeController;
 
 
@@ -203,11 +202,8 @@ Route::prefix('/project/{projectId}')->group(function () {
 
     // start of the reporting routes
 
-     Route::get('/reporting/', [ReportingController::class, 'index'])->name('reporting.index')->middleware('auth')->middleware(Localization::class)
+     Route::get('/reporting/', [ReportingController::class, 'index'])->name('reporting.index')->middleware('auth')->middleware(Localization::class);
 
-    // Star of Conducting routes
-    Route::get('/conducting/', [ConductingController::class, 'index'])->name('project.conducting.index')->middleware('auth')->middleware(Localization::class);
-    // End of Conducting routes
 
 });
 
