bf6181fbbc1d035c9c363234b5416f8bdd5154ff
bianca <biancamr.aluno@unipampa.edu.br>
2024-07-07 22:21:30 -0300

    alterações

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -12,9 +12,6 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        Level::create(['name' => 'Administrador', 'permissions' => json_encode(['create projects', 'edit projects', 'delete projects', 'view projects', 'manage users'])]);
-        Level::create(['name' => 'Pesquisador', 'permissions' => json_encode(['create projects', 'edit projects', 'view projects'])]);
-        Level::create(['name' => 'Revisor', 'permissions' => json_encode(['view projects'])]);
-        Level::create(['name' => 'Visualizador', 'permissions' => json_encode(['view projects'])]);
+
     }
 }
