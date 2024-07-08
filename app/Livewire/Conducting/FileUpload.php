556c5ee1bc1898b2c2aada8e799036efba03fd1b
GABRIELE_SA\bruxa <gabiagostini16@gmail.com>
2024-07-08 20:09:49 -0300

    FileUpload funcionando

--- app/Livewire/Conducting/FileUpload.php
+++ app/Livewire/Conducting/FileUpload.php
@@ -2,11 +2,11 @@
 
 namespace App\Livewire\Conducting;
 
+use App\Utils\ToastHelper;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use App\Models\File;
 use App\Models\Project as ProjectModel;
-
 
 class FileUpload extends Component
 {
@@ -14,50 +14,70 @@ class FileUpload extends Component
 
   public $currentProject;
   public $databases = [];
-  public $selectedDatabase = '';
+  public $selectedDatabase = [];
 
   public $file;
 
+  protected $messages = [
+    'selectedDatabase.required' => 'The database field is required.',
+    'file.required' => 'The file field is required.',
+    'file.mimes' => 'The file must be a type of: bib, csv.',
+    'file.max' => 'The file size must not exceed 10MB.',
+  ];
+
+  protected $rules = [
+    'selectedDatabase' => 'required|exists:project_databases,id_database',
+    'file' => 'required|file|max:10240',
+    'file.mimes' => 'The file must be a type of: bib, csv.',
+    'file.max' => 'The file size must not exceed 10MB.',
+  ];
+
+  public function toast(string $message, string $type)
+  {
+    $this->dispatch('file-upload', ToastHelper::dispatch($type, $message));
+  }
+
   public function mount()
   {
-      $projectId = request()->segment(2);
-      $this->currentProject = ProjectModel::findOrFail($projectId);
-      $this->databases = $this->currentProject->databases;
-   }
+    $projectId = request()->segment(2);
+    $this->currentProject = ProjectModel::findOrFail($projectId);
+    $this->databases = $this->currentProject->databases;
+  }
 
-    /**
-     * Reset the fields to the default values.
-     */
-   public function resetFields()
-   {
-       $this->selectedDatabase = '';
-       $this->file = null;
-   }
+  /**
+   * Reset the fields to the default values.
+   */
+  public function resetFields()
+  {
+    $this->selectedDatabase['value'] = null;
+    $this->file = null;
+  }
 
-   public function save()
-   {
-       $this->validate([
-           'selectedDatabase' => 'required|exists:project_databases,id_database',
-           'file' => 'required|file|mimes:bib,csv|max:10240',
-       ]);
+  public function save()
+  {
+    $this->validate();
 
-       $name = md5($this->file . microtime()) . '.' . $this->file->extension();
+    $name = md5($this->file . microtime()) . '.' . $this->file->extension();
 
-       $this->file->storeAs('files', $name);
+    $this->file->storeAs('files', $name);
 
-       File::create([
-           'file_name' => $name,
-           'id_database' => $this->selectedDatabase,
-       ]);
+    File::create([
+      'file_name' => $name,
+      'id_database' => $this->selectedDatabase['value'],
+    ]);
 
-       session()->flash('message', 'The file is successfully uploaded!');
-   }
+    $this->toast(
+      message: 'File uploaded successfully.',
+      type: 'success'
+    );
 
+    $this->resetFields();
+  }
 
-   public function render()
-   {
-     return view('livewire.conducting.file-upload', [
-       'files' => File::all(),
-     ]);
-   }
- }
+  public function render()
+  {
+    return view('livewire.conducting.file-upload', [
+      'files' => File::all(),
+    ]);
+  }
+}
--- resources/views/livewire/conducting/file-upload.blade.php
+++ resources/views/livewire/conducting/file-upload.blade.php
@@ -1,144 +1,106 @@
 <div class="d-flex flex-column gap-4">
     <div class="card">
         <div class="card-header mb-0 pb-0">
-            <x-helpers.modal target="import-studies" modalTitle="{{ __('project/conducting.import-studies.title') }}" modalContent="{{ __('project/conducting.import-studies.help.content') }}" />
+            <x-helpers.modal
+                target="import-studies"
+                modalTitle="{{ __('project/conducting.import-studies.title') }}"
+                modalContent="{{ __('project/conducting.import-studies.help.content') }}"
+            />
         </div>
         <div class="card-body">
-            <form wire:submit.prevent="save" class="d-flex flex-column">
-                <div class="d-flex flex-column gap-2 form-group">
-                    <x-select class="w-md-25 w-100" id="databaseSelect" label="{{ __('project/conducting.import-studies.form.selected-database') }}" wire:model="selectedDatabase">
-                        <option value="">
-                            {{ __("project/conducting.import-studies.form.selected-database") }}
-                        </option>
-                        @foreach ($databases as $database)
-                        <option value="{{ $database->id_database }}">
+            <div class="d-flex flex-column gap-2 form-group">
+                <x-select
+                    class="w-md-25 w-50"
+                    id="databaseSelect"
+                    label="{{ __('project/conducting.import-studies.form.selected-database') }}"
+                    wire:model="selectedDatabase"
+                >
+                    <option selected disabled>
+                        {{ __("project/conducting.import-studies.form.selected-database") }}
+                    </option>
+                    @foreach ($databases as $database)
+                        <option
+                            value="{{ $database->id_database }}"
+                            <?= ($selectedDatabase["value"] ?? "") == $database->id_database     ? "selected"     : "" ?>
+                        >
                             {{ $database->name }}
                         </option>
-                        @endforeach
-                    </x-select>
-                    @error("selectedDatabase")
+                    @endforeach
+                </x-select>
+                @error("selectedDatabase")
                     <span class="text-xs text-danger">
                         {{ $message }}
                     </span>
-                    @enderror
+                @enderror
 
-                    <div class="d-flex flex-column">
-                        <label for="fileUpload" class="form-control-label mx-0 mb-1">
+                <div class="d-flex flex-column">
+                    <form wire:submit="save">
+                        <label
+                            for="fileUpload"
+                            class="form-control-label mx-0 mb-1"
+                        >
                             {{ __("project/conducting.import-studies.form.upload") }}
                         </label>
-                        <input type="file" class="form-control" id="fileUpload" wire:model="file" accept=".bib,.csv" />
-                    </div>
-                    @error("file")
-                    <span class="text-xs text-danger">
-                        {{ $message }}
-                    </span>
-                    @enderror
-                </div>
-                <x-helpers.submit-button>
-                    {{ __("project/conducting.import-studies.form.add") }}
-                    <div wire:loading>
-                        <i class="fas fa-spinner fa-spin"></i>
-                    </div>
-                </x-helpers.submit-button>
-            </form>
+                        <input
+                            type="file"
+                            class="form-control"
+                            id="fileUpload"
+                            wire:model="file"
+                            accept=".bib,.csv"
+                        />
+                        @error("file")
+                            <span class="text-xs text-danger">
+                                {{ $message }}
+                            </span>
+                        @enderror
 
-            <div class="mt-3">
-                @if (session()->has("message"))
-                <div class="alert alert-success">
-                    {{ session("message") }}
+                        <x-helpers.submit-button
+                            class="mt-3"
+                            type="submit"
+                            wire:loading.attr="disabled"
+                        >
+                            Enviar arquivo
+                        </x-helpers.submit-button>
+                    </form>
                 </div>
-                @endif
-
-                @if (session()->has("error"))
-                <div class="alert alert-danger">
-                    {{ session("error") }}
-                </div>
-                @endif
             </div>
-
             <hr style="opacity: 10%" />
-
             <div class="overflow-auto px-2 py-1" style="max-height: 300px">
-                <table class="table table-responsive table-hover" id="study-import-table">
-                    <thead class="table-light sticky-top custom-gray-text" style="color: #676a72">
-                        <th style="
-                            border-radius: 0.75rem 0 0 0;
-                            padding: 0.5rem 1rem;
-                        ">
+                <table class="table table-responsive table-hover">
+                    <thead
+                        class="table-light sticky-top custom-gray-text"
+                        style="color: #676a72"
+                    >
+                        <th
+                            style="
+                                border-radius: 0.75rem 0 0 0;
+                                padding: 0.5rem 1rem;
+                            "
+                        >
                             {{ __("project/conducting.import-studies.table.database") }}
-                            <i class="fas fa-sort"></i>
                         </th>
                         <th style="padding: 0.5rem 0.75rem">
                             {{ __("project/conducting.import-studies.table.studies-imported") }}
-                            <i class="fas fa-sort"></i>
-                        </th>
-                        <th class="text-center" style="border-radius: 0 0.75rem 0 0; padding: 0.5rem 0.75rem;">
-                            {{ __("project/conducting.import-studies.table.file-imported") }}
                         </th>
                     </thead>
                     <tbody>
                         @foreach ($databases as $database)
-                        <tr>
-                            <td class="align-middle">{{ $database->name }}</td>
-                            <td class="align-middle">{{ $database->imported_study_count }}</td>
-                            <td>
-                                <table class="table table-responsive table-hover">
-                                    <thead>
-                                        <th style="border-radius: 0.75rem 0 0 0; padding: 0.5rem 1rem;">
-                                            {{ __("project/conducting.import-studies.table.file") }}
-                                        </th>
-                                        <th class="text-center" style="border-radius: 0 0.75rem 0 0; padding: 0.5rem 1rem;">
-                                            {{ __("project/conducting.import-studies.table.delete") }}
-                                        </th>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($files as $file)
-                                        <tr>
-                                            <td>{{ $file->file_name }}</td>
-                                            <td class="text-center">
-                                                <button class="btn py-1 px-3 btn-outline-danger" wire:click="confirmDelete('{{ $file->id }}')" wire:loading.attr="disabled">
-                                                    <i class="fas fa-trash"></i>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
+                            <tr>
+                                <td>{{ $database->name }}</td>
+                                <td></td>
+                            </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+</div>
 
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tablesort/5.2.1/tablesort.min.js"></script>
+@script
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            new Tablesort(document.getElementById('study-import-table'));
-
-            // Filtragem da tabela
-            const searchInput = document.getElementById('search-input');
-            const table = document.getElementById('study-import-table').getElementsByTagName('tbody')[0];
-            const rows = table.getElementsByTagName('tr');
-
-            searchInput.addEventListener('keyup', function() {
-                const filter = searchInput.value.toLowerCase();
-
-                for (let i = 0; i < rows.length; i++) {
-                    const cells = rows[i].getElementsByTagName('td');
-                    let display = false;
-                    for (let j = 0; j < cells.length; j++) {
-                        if (cells[j].textContent.toLowerCase().indexOf(filter) > -1) {
-                            display = true;
-                            break;
-                        }
-                    }
-                    rows[i].style.display = display ? '' : 'none';
-                }
-            });
+        $wire.on('file-upload', ([{ message, type }]) => {
+            toasty({ message, type });
         });
     </script>
-</div>
+@endscript
