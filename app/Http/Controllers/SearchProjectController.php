b192f46ea2c2e95cc39fb91a38910c826df2987f
Ricardo Costa <ricardo_hentges@hotmail.com>
2024-07-08 19:35:00 -0300

    fix: :bug: Adição de query para retornar apenas projetos do usuário
    Para retornar apenas projetos que o usuário está envolvido, foi preciso adicionar o id do usuário à consulta, retornando apenas os projetos que o usuário está envolvido.
    
    #350

--- app/Http/Controllers/SearchProjectController.php
+++ app/Http/Controllers/SearchProjectController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
 
 class SearchProjectController extends Controller
 {
     public function searchByTitleOrCreated(Request $request)
     {
+        $user = Auth::user();
         $searchProject = $request->searchProject;
-        $projects = Project::where('title', 'like', '%' . $searchProject . '%')
-                            ->orWhere('created_by', 'like', '%' . $searchProject . '%')
+
+        $projects = Project::whereHas('users', function($query) use ($user) {
+                                $query->where('id_user', $user->id);
+                            })
+                            ->where(function($query) use ($searchProject) {
+                                $query->where('title', 'like', '%' . $searchProject . '%')
+                                    ->orWhere('created_by', 'like', '%' . $searchProject . '%');
+                            })
                             ->get();
 
         return view('projects.search-project', compact('projects', 'searchProject'));
