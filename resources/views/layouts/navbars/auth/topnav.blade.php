aa8b86ea2fb6756ecd86f94e141590931b0dddb3
Diego Comis <diegocomis@gmail.com>
2024-11-11 10:18:09 -0300

    Security API KEY - Ajuste Politica de privacidade - Register Controller

--- resources/views/layouts/navbars/auth/topnav.blade.php
+++ resources/views/layouts/navbars/auth/topnav.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl
         {{ str_contains(Request::url(), 'virtual-reality') == true ? ' mt-3 mx-3 bg-primary' : '' }}"
     id="navbarBlur" data-scroll="false">
-    <div class="container-fluid py-1 px-3">
+    <div id="top" class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                 <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">{{ __('nav/nav.pages') }}</a></li>
