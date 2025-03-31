dbec2418560dfa2b6526032cd19d98e32e4031b4
Camilla <kamysbdt@gmail.com>
2025-03-31 20:06:20 -0300

    fazendo creates e tentando entender o sistema

--- app/Utils/ActivityLogHelper.php
+++ app/Utils/ActivityLogHelper.php
@@ -10,13 +10,12 @@ class ActivityLogHelper
     public static function insertActivityLog($activity, $id_module, $id_project, $id_user = null)
     {
        
-    // Desativado temporariamente por falta de módulo no banco
-    // Activity::create([
-    //     'activity' => $descricao,
-    //     'id_module' => 1,
-    //     'id_project' => $idProjeto,
-    //     'id_user' => Auth::id(),
-    // ]);
+    //Activity::create([
+    //'activity' => $descricao,
+    //'id_module' => 1,
+       //'id_project' => $idProjeto,
+     //'id_user' => Auth::id(),
+    //]);
 
     }
     
