dca469827ab0fd505e38ed483e0948f90b2a491d
Camilla <kamysbdt@gmail.com>
2025-04-07 01:03:12 -0300

    adicionando o botão de configuração para guest

--- resources/views/layouts/navbars/guest/navbar.blade.php
+++ resources/views/layouts/navbars/guest/navbar.blade.php
@@ -141,6 +141,12 @@
                                         </a>
                                     </form>
                                 </li>
+                                <li class="nav-item px-3 d-flex align-items-center">
+                                  <a href="javascript:;" class="nav-link text-white p-0">
+                                    <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
+                                  </a>
+                                </li>
+
                             @endauth
                         </ul>
                         <!-- Language Selector Dropdown -->
