582e34113074fa10ad258b13af26258bc86f983e
Júlio Silveira <julio.silveira@ayvu.net>
2025-05-12 14:39:46 -0300

    feat: data export button on activity report modal

--- lang/en/project/overview.php
+++ lang/en/project/overview.php
@@ -16,5 +16,9 @@ return [
     'data_extraction' => 'Data Extraction',
 
     'activity_record' => 'Activity Record',
-
+    'view_full_history' => 'View full history',
+    'full_activity_history' => 'Full activity history',
+    'no_activities' => 'No activities recorded.',
+    'export' => 'Export data',
+    'close' => 'Close',
 ];
