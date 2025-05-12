9e1ba4e177a4d4ea83284090e1a72a3d1bc7f770
Miguel Arrojo <Miguelarrojo.aluno@Unipampa.edu.br>
2025-05-12 18:29:24 -0300

    fix: navbar fixa corretamente no topo da tela

--- resources/views/layouts/navbars/guest/navbar.blade.php
+++ resources/views/layouts/navbars/guest/navbar.blade.php
@@ -1,8 +1,8 @@
-<div class="container position-sticky z-index-sticky top-0">
+<div class="container-fluid">
     <div class="row">
         <div class="col-12">
             <!-- Navbar -->
-            <nav class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0 mx-4">
+            <nav class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-fixed start-0 end-0 mx-4">
                 <div class="container-fluid">
                     <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3" href="{{ route('home') }}">
                         <img src="/img/logo.svg" alt="{{ __('Logo Thoth') }}" width="25" height="35" />
