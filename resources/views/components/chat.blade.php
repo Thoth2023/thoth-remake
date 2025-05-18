79390d4316199d151afc22191e026b979310287e
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2025-05-18 12:51:18 -0300

    feat: ajustes no redimensionamento do chat flutuante e implementação de atalho de teclado
    feat: ajustes no redimensionamento do chat flutuante e implementação de atalho de teclado
    
    - Chat agora identifica e exibe o nome do usuário autenticado ao enviar mensagens
    - Redimensionamento aumenta a janela, fonte das mensagens e campo de entrada
    - Atalho de teclado Alt + C permite expandir/recolher o chat com praticidade

--- resources/views/components/chat.blade.php
+++ resources/views/components/chat.blade.php
@@ -2,20 +2,22 @@
 b
 
 <div id="chat-container" style="position:fixed; bottom:20px; right:20px; width:320px; z-index:9999; font-family:inherit;">
-    <div id="chat-header" class="bg-gradient-faded-dark text-white p-3 rounded-top shadow cursor-pointer d-flex justify-content-between align-items-center">
+    <div id="chat-header" style="display: flex;" class="bg-gradient-faded-dark text-white p-3 rounded-top shadow cursor-pointer d-flex justify-content-between align-items-center">
         <span>Chat do Projeto</span>
         <div class="d-flex align-items-center gap-2">
-            <span id="chat-notif" class="bg-danger text-white px-2 py-1 rounded-circle d-none">!</span>
-            <button id="resize-btn" class="btn btn-sm btn-light text-dark" style="padding: 2px 8px; font-size: 12px;">
-                ⤢
-            </button>
+            <span id="chat-notif" class="bg-danger text-white px-2 py-1 rounded-circle d-none" style="margin-right: 8px;">!</span>
+                <button id="resize-btn"
+                    class="btn btn-sm btn-light text-dark d-flex justify-content-center align-items-center"
+                    style="width: 32px; height: 32px; padding: 0; font-size: 20px; line-height: 1; margin-top: 8px">
+                    ⤢
+                </button>
         </div>
     </div>
 
 
     <div id="chat-body" class="bg-white border rounded-bottom shadow chat-small" style="display:none;">
-        <div id="chat-box" class="p-3" style="height:180px; overflow-y: auto; font-size:14px;"></div>
-        <div style="padding: 10px;">
+        <div id="chat-box" class="p-3" style="overflow-y: auto; font-size:14px;"></div>
+        <div id="chat-input-area" style="padding: 10px;">
             <div style="display: flex; gap: 5px; margin-top: 5px;">
                 <textarea id="mensagem" placeholder="Digite sua mensagem..."
                     class="form-control"
@@ -58,6 +60,7 @@ const projetoId = "{{ $projeto_id }}";
         if (chatAberto) {
             chatNotif.classList.add('d-none');
             carregarMensagens();
+            ajustarAlturaChatBox()
         }
     });
 
@@ -106,26 +109,58 @@ const projetoId = "{{ $projeto_id }}";
     carregarMensagens();
 
 
+    //Atalho Alt + C
+    document.addEventListener('keydown', function(event) {
+        if (event.altKey && event.key.toLowerCase() === 'c') {
+            chatHeader.click();
+        }
+    });
+
+
+
+
     //Redimencionar janela
     const resizeBtn = document.getElementById('resize-btn');
     let isExpanded = false;
 
     resizeBtn.addEventListener('click', () => {
         const chatContainer = document.getElementById('chat-container');
+        //const chatHeader = document.getElementById('chat-header');
         const chatBody = document.getElementById('chat-body');
+        const chatBox = document.getElementById('chat-box');
+        const mensagemInput = document.getElementById('mensagem');
 
         isExpanded = !isExpanded;
 
         if (isExpanded) {
             chatContainer.style.width = '500px';
+            //charHeader.style.width = '500px';
             chatBody.classList.remove('chat-small');
             chatBody.classList.add('chat-large');
+            chatBox.style.fontSize = '22px';
+            mensagemInput.style.fontSize = '22px';
         } else {
             chatContainer.style.width = '320px';
+            //charHeader.style.width = '320px';
             chatBody.classList.remove('chat-large');
             chatBody.classList.add('chat-small');
+            chatBox.style.fontSize = '14px';
+            mensagemInput.style.fontSize = '14px';
         }
+
+        ajustarAlturaChatBox();
     });
+
+    function ajustarAlturaChatBox() {
+        const container = document.getElementById('chat-container');
+        const header = document.getElementById('chat-header');
+        const inputArea = document.getElementById('chat-input-area');
+        const chatBox = document.getElementById('chat-box');
+
+        // Altura total do container menos cabeçalho e campo de envio
+        const alturaDisponivel = container.offsetHeight - header.offsetHeight - inputArea.offsetHeight;
+        chatBox.style.height = `${alturaDisponivel}px`;
+    }
 
 
 </script>
