68f4a5e34dbb99031b939e1cfb7e2cf3277c0c7c
Diego Comis <diegocomis@gmail.com>
2025-10-09 05:49:13 -0300

    ajuste nas extensões e nos mime types na importação de estudos

--- app/Rules/ValidBibFile.php
+++ app/Rules/ValidBibFile.php
@@ -8,20 +8,12 @@ class ValidBibFile implements Rule
 {
     public function passes($attribute, $value)
     {
-        if (!$value) {
-            return false;
-        }
-
-        $mimeType = $value->getMimeType();
         $extension = $value->getClientOriginalExtension();
-
-        // Verifica se é um arquivo de texto e tem extensão .bib ou .csv
-        return (str_starts_with($mimeType, 'text/') || $mimeType === 'application/csv') &&
-               in_array($extension, ['bib', 'csv','txt']);
+        return in_array($extension, ['bib', 'csv', 'txt']);
     }
 
     public function message()
     {
-        return 'Apenas arquivos .bib, .txt(formato BIB) e .csv são permitidos.';
+        return __("project/conducting.import-studies.bib_file");
     }
 }
