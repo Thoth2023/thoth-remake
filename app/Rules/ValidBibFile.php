8a6e597176f9d7541720dfd268f874182a2aab58
Diego Comis <diegocomis@gmail.com>
2025-10-09 00:39:02 -0300

    ajuste para deixar importar .txt no formato BIB e arquivo com DOI em branco

--- app/Rules/ValidBibFile.php
+++ app/Rules/ValidBibFile.php
@@ -14,14 +14,14 @@ class ValidBibFile implements Rule
 
         $mimeType = $value->getMimeType();
         $extension = $value->getClientOriginalExtension();
-        
+
         // Verifica se é um arquivo de texto e tem extensão .bib ou .csv
-        return (str_starts_with($mimeType, 'text/') || $mimeType === 'application/csv') && 
-               in_array($extension, ['bib', 'csv']);
+        return (str_starts_with($mimeType, 'text/') || $mimeType === 'application/csv') &&
+               in_array($extension, ['bib', 'csv','txt']);
     }
 
     public function message()
     {
-        return 'Apenas arquivos .bib e .csv são permitidos.';
+        return 'Apenas arquivos .bib, .txt(formato BIB) e .csv são permitidos.';
     }
 }
